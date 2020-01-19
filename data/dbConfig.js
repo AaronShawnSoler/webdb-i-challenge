b0681f68a1f1f6f885289dedc44e1c8e138e7236
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-01-19 00:12:54 -0500

    relational database

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -5,9 +5,39 @@ const configOptions = require('../knexfile').development;
 const db = knex(configOptions);
 
 module.exports = {
-    get
+    get,
+    getById,
+    insert,
+    update,
+    remove
 }
 
 function get() {
     return db('accounts');
+}
+
+function getById(id) {
+    return db('accounts')
+    .where({ id })
+    .first();
+}
+
+function insert(account) {
+    return db('accounts')
+    .insert(account)
+    .then(ids => {
+        return getById(ids[0]);
+    });
+}
+
+function update(accountID, changes) {
+    return db('accounts')
+    .where({ id: accountID })
+    .update(changes);
+}
+
+function remove(id) {
+    return db('accounts')
+    .where('id', id)
+    .del();
 }
--- index.js
+++ index.js
@@ -8,6 +8,58 @@ server.get('/', (req, res) => {
   .then(response => res.send(response));
 })
 
+server.get('/:id', (req, res) => {
+  db.getById(req.params.id)
+  .then(data => {
+    if(data) {
+      res.send(data);
+    } else {
+      res.status(404).json({ message: 'account not found'});
+    }
+  })
+})
+
+server.post('/', (req, res) => {
+  if (req.body.name && req.body.budget) {
+    const newAccount = {
+      name: req.body.name,
+      budget: req.body.budget
+    }
+    db.insert(newAccount);
+    res.status(201).send(newAccount);
+  } else {
+    res.status(400).json({ message: 'please enter name and budget for account'});
+  }
+})
+
+server.put('/:id', (req, res) => {
+  const id = req.params.id;
+  try {
+    db.getById(id)
+    .then(account => {
+      if(account) {
+        db.update(id, req.body)
+        .then(response => res.status(204).send());
+      } else {
+        res.status(404).json({ message: 'account not found'});
+      }
+    })
+  } catch {
+    res.status(500).json({ error: 'error'})
+  }
+});
+
+server.delete('/:id', (req, res) => {
+  db.remove(req.params.id)
+  .then(data => {
+    if(data) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'account not found'});
+    }
+  })
+})
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
